ced4c4aa928290a38778300575013b5c8ac6531b
lipinghe <624653767@qq.com>
2021-01-22 14:39:20 +0800

    2.登录模块-基础布局

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// @代表src的绝对路径，只有在vue-cli中才可以用
+import router from '@/router'
+
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="contain">
+        <!-- 卡片 -->
+        <el-card class="box-card">
+             <!-- 1.头部图片 -->
+            <img src="../../assets/images/logo_index.png" alt="">
+             <!-- 2.表单 -->
+            <el-form  :model="loginForm" >
+                <!-- 手机号 -->
+                <el-form-item >
+                     <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+                </el-form-item>
+                <!-- 验证码 -->
+                 <el-form-item >
+                     <el-input v-model="loginForm.code" placeholder="请输入验证码" class="code"></el-input>
+                     <el-button class="vertify">发送验证码</el-button>
+                </el-form-item>
+                <!-- 隐私条款 -->
+                <el-form-item >
+                      <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+                </el-form-item>
+                <!-- 提交 -->
+                <el-form-item >
+                       <el-button type="primary" style="width:100%">提交</el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
+</script>
+
+<style  scoped lang="less">
+.contain{
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left:0;
+    top:0;
+    background: url('../../assets/images/login_bg.jpg') no-repeat center/cover;
+    .box-card{
+        width:400px;
+        position: absolute;
+        left:50%;
+        top:50%;
+        transform: translate(-50%,-50%);
+        img{
+            display: block;
+            margin:0 auto 20px;//不设置display: block;不生效
+        }
+        .code{
+            width:200px;
+            margin-right: 30px;
+        }
+        .vertify{
+            height: 100%;
+        }
+    }
+}
+</style>
